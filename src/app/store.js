5519921882f8973e979e3774fcd4082ca9c8652a
kalai02484 <kalai02484@gmail.com>
2026-09-07 00:55:35 +0530

    Redux store AuthSlice added and Store connected to main.jsx

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,14 @@ import authReducer from "../features/auth/authSlice";
 import restaurantReducer from "../features/restaurant/restaurantSlice";
 import reservationReducer from "../features/reservation/reservationSlice";
 import reviewReducer from "../features/review/reviewSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    restaurant: restaurantReducer,
-    reservation: reservationReducer,
-    review: reviewReducer,
+    restaurants: restaurantReducer,
+    reservations: reservationReducer,
+    reviews: reviewReducer,
+    user: userReducer,
   },
 });
