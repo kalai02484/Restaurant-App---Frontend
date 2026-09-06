aa6425934d86d352e53d3cf8e9953b7a2041fe21
kalai02484 <kalai02484@gmail.com>
2026-09-06 13:14:42 +0530

    Api added

---
+++ src/api/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const api = axios.create({
+  baseURL: process.env.VITE_API_URL || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default api;
